6b435355166305bdc40b84bd49b2aa18415af1b5
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-04-09 12:05:54 +0100

    Bump wx_icons_tango version: 0.0.0 → 0.1.0

--- tango/wx_icons_tango/__init__.py
+++ tango/wx_icons_tango/__init__.py
@@ -12,7 +12,7 @@ from wx_icons_tango import Tango
 with importlib_resources.path(Tango, "index.theme") as theme_index_path:
 	theme_index_path = str(theme_index_path)
 
-__version__ = "0.0.0"
+__version__ = "0.1.0"
 
 
 def version():
