dc497377a3e1397a51a8321f005f53e0a7c112eb
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-04-10 22:22:20 +0100

    Added script to create boilerplate for a custom icon theme

--- hicolor/build_icons_from_src.py
+++ hicolor/build_icons_from_src.py
@@ -9,9 +9,8 @@ This takes around 15 minutes to run so be patient.
 #  Copyright (C) 2020 Dominic Davis-Foster <dominic@davis-foster.co.uk>
 #
 #  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU Lesser General Public License as published by
-#  the Free Software Foundation; either version 3 of the License, or
-#  (at your option) any later version.
+#  it under the terms of the GNU Lesser General Public License version 3
+#  as published by the Free Software Foundation.
 #
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
