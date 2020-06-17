03f13e83f304b222df6b010ae3a51d7c3cf7435a
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-06-17 11:22:51 +0000

    Updated files with `git_helper`.

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -89,8 +89,7 @@ classifiers = [
 		'Programming Language :: Python',
 		'Programming Language :: Python :: 3 :: Only',
 		'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-		'Operating System :: Microsoft :: Windows',
-		'Operating System :: MacOS',
+		'Operating System :: OS Independent',
 
 		]
 
